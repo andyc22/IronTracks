20ba45c419092e0aad0c9139ef16495f3f28a295
andyc22 <70164106+andyc22@users.noreply.github.com>
2025-06-04 16:59:54 +0100

    Expose attack info on cards

--- PTCGLDeckTracker.Tests/CardTests.cs
+++ PTCGLDeckTracker.Tests/CardTests.cs
@@ -1,4 +1,5 @@
 using PTCGLDeckTracker.CardCollection;
+using System.Collections.Generic;
 using Xunit;
 
 namespace PTCGLDeckTracker.Tests;
@@ -10,6 +11,19 @@ public class CardTests
     {
         string result = Card.GetEnglishNameFromCard3DName("Card3D_Pikachu");
         Assert.Equal("Pikachu", result);
+    }
+
+    [Fact]
+    public void Card_AttackFields_CanBeAssigned()
+    {
+        var card = new Card("abc")
+        {
+            AttackDamage = new List<string> { "20" },
+            EnergyRequirement = new List<string> { "L" }
+        };
+
+        Assert.Equal("20", card.AttackDamage[0]);
+        Assert.Equal("L", card.EnergyRequirement[0]);
     }
 }
 
